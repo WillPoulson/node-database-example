4c484786d826e9bb928e19a0117c616678a27c17
Will Poulson <willpoulson44@gmail.com>
2019-05-31 23:25:14 +0100

    Added the database query and some basic validation.

--- routes.js
+++ routes.js
@@ -1,8 +1,15 @@
 var router = require('express').Router();
 
+const { check, validationResult } = require('express-validator/check');
 const store = require('./store');
 
-router.post('/createUser', (req, res) => {
+router.post('/createUser', [
+    check('username')
+        .exists().withMessage('Username is required'),
+    check('password')
+        .exists().withMessage('Password is required')
+        .isLength({min: 5}).withMessage('Password must be atleast 5 chars')
+], (req, res) => {
     store.createUser({
         username: req.body.username,
         password: req.body.password
--- store.js
+++ store.js
@@ -1,6 +1,11 @@
+const knex = require('knex')(require('./knexfile'));
+
 module.exports = {
     createUser ({username, password}) {
         console.log(`Received request to save user ${username} ${password}.`);
-        return Promise.resolve();
+        return knex('user').insert({
+            username,
+            password
+        })
     }
 }
