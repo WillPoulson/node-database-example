ebce4970685faf01b9b853d4a04bda027650493b
Will Poulson <willpoulson44@gmail.com>
2019-06-01 00:14:18 +0100

    Added an authenticate route.

--- routes.js
+++ routes.js
@@ -26,4 +26,31 @@ router.post('/createUser', [
     })
 })
 
+router.post('/authenticate', [
+    check('username')
+        .exists().withMessage('Username is required'),
+    check('password')
+        .exists().withMessage('Password is required')
+        .isLength({min: 5}).withMessage('Password must be atleast 5 chars')
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+
+    store.authenticate({
+        username: req.body.username,
+        password: req.body.password
+    }).then(() => {
+        return res.sendStatus(200);
+    }).catch((error) => {
+        if(error = 'Incorrect password' || error == 'User not found') {
+            return res.send('Incorrect username or password').status(404);
+        } else {
+            console.log(error);
+            return res.sendStatus(500);
+        }
+    })
+})
+
 module.exports = router;
--- store.js
+++ store.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     saltHashPassword,
-    createUser
+    createUser,
+    authenticate
 }
 
 async function createUser ({username, password}) {
@@ -13,8 +14,39 @@ async function createUser ({username, password}) {
 
     return knex('user').insert({
         salt,
-        encrypted_passwor: hash,
+        encrypted_password: hash,
         username
+    })
+}
+
+async function authenticate ({username, password}) {
+    console.log(`Authenticating ${username}`);
+    const [user] = await knex('user').where({username});
+
+    if (!user) {
+        throw (new Error('User not found'));
+    }
+
+    console.log(user);
+    console.log(user.encrypted_password);
+
+    const match = await comparePassword(password, user.encrypted_password);
+
+    if (!match) {
+        throw (new Error('Incorrect password'));
+    }
+
+    return match;
+}
+
+function comparePassword (password, hash) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, hash, (err, match) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(match);
+        })
     })
 }
 
